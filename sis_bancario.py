3b24dc496c9c8a990e44f1a88173ab2142cde024
João Magalhães <joaovictorm0103@gmail.com>
2024-05-19 21:11:26 -0300

    refactor: moudlarização em funções

--- sis_bancario.py
+++ sis_bancario.py
@@ -1,66 +1,82 @@
-"""
-REGRAS DE NEGOCIO
-Implementar apenas as 3 operações básicas (sacar, depositar e ver extrato)
-*Depositar: Não é exigido verificao de usuário.
-*Sacar: máximo de 3 saques por dia, máximo de 500.00 por saque, não possui cheque especial.
-*Extrato: Exibir um extrato da conta e o saldo no final, caso não tenha sido efetuado transações exibir
-        "Não foram realizadas movimentações."
-"""
-
 saldo = 1000
 limite = 500
 numero_de_saques = 0
 LIMITE_SAQUES = 3
 extrato = ""
 
+
+#funcao Depositar
+
+def deposito(saldo, valor, extrato,/):
+    if valor < 0:
+        print("Valor não aceito para depósito.")
+
+    else:    
+        saldo += valor
+        extrato += f"Depósito realizado no valor de R$ {valor:.2f}\n"
+
+    return saldo, extrato
+
+def saque(*, saldo, valor, extrato, limite, numero_de_saques, limite_saques):
+    if valor > limite:
+        print("Valor de saque excede o limite por saque.")
+        
+    elif valor > saldo:
+        print("Saldo insuficiente.")
+        
+    elif numero_de_saques >= limite_saques:
+        print("Número de saques diários excedido.")
+
+    elif valor <= 0:
+        print("Valor de saque não é aceito.")    
+
+    else:
+        saldo -= valor
+        numero_de_saques += 1
+        extrato += f"Saque realizado no no valor de R$ {valor:.2f}\n"
+        print("Saque realizado com sucesso!\n")
+
+    return saldo, extrato, numero_de_saques
+
+def exibe_extrato(saldo,/,*, extrato):
+    if extrato == "":
+        print("Não há movimentações até o momento.\n")
+    else:
+        print("#####EXTRATO#####\n")
+        print(extrato)
+        print(f"Saldo: {saldo}")
+
+    return
+
+
 menu = """
-########## MENU ##########
-0 - Realizar saque;
-1 - Realizar depósito;
-2 - Ver extrato;
-3 - Sair.
+    ########## MENU ##########
+    0 - Realizar saque;
+    1 - Realizar depósito;
+    2 - Ver extrato;
+    3 - Sair.
 """
 opcao = int(input(menu))
 
 while(True):
     #SAQUE
     if opcao == 0:
-        valor = float(input("Qual o valor do saque desejado?\n"))
-        if valor > limite:
-            print("Valor de saque excede o limite po saque")
-            
-        elif valor > saldo:
-            print("Saldo insuficiente.")
-            
-        elif numero_de_saques > LIMITE_SAQUES:
-            print("Número de saques diários excedido.")
-
-        elif valor < 0:
-            print("Valor de saque não é aceito.")    
-
-        else:
-            saldo -= valor
-            numero_de_saques += 1
-            extrato += f"Saque realizado no no valor de R$ {valor:.2f}\n"
-
-    #Depósito
+        valor = float(input("Qual valor do saque?\n"))
+        retorno = saque(saldo = saldo, valor = valor, extrato = extrato, limite = limite, numero_de_saques = numero_de_saques, limite_saques = LIMITE_SAQUES)
+        saldo = retorno[0]
+        extrato = retorno[1]
+        numero_de_saques = retorno[2]
+        
+    #DEPOSITO
     if opcao == 1:
-        valor = float(input("Qual valor do depósito?\n"))
-        if valor < 0:
-            print("Valor não aceito para depósito.")
-
-        else:    
-            saldo += valor
-            extrato += f"Depósito realizado no valor de R$ {valor:.2f}\n"
+        valor = float(input("Qual valor do deposito?\n"))
+        retorno = deposito(saldo, valor, extrato)
+        saldo = retorno[0]
+        extrato = retorno[1]
 
     #EXTRATO
     if opcao == 2:
-        if extrato == "":
-            print("Não há movimentações até o momento.")
-        else:
-            print("#####EXTRATO#####\n")
-            print(extrato)
-            print(f"Saldo: {saldo}")
+        exibe_extrato(saldo, extrato = extrato)
 
     #SAIR
     if opcao == 3:
@@ -68,3 +84,5 @@ while(True):
         break
     
     opcao = int(input(menu))
+
+
