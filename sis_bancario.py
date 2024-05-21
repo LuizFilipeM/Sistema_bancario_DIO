fb759318ed03a5d2fa802d2e8de65153550fc51e
João Magalhães <joaovictorm0103@gmail.com>
2024-05-20 21:54:00 -0300

    feat: funcoes de cadastrar cliente e criar conta corrente adicionadas

--- sis_bancario.py
+++ sis_bancario.py
@@ -4,9 +4,6 @@ numero_de_saques = 0
 LIMITE_SAQUES = 3
 extrato = ""
 
-
-#funcao Depositar
-
 def deposito(saldo, valor, extrato,/):
     if valor < 0:
         print("Valor não aceito para depósito.")
@@ -48,41 +45,76 @@ def exibe_extrato(saldo,/,*, extrato):
 
     return
 
+def cadastra_usuario(usuarios):
+    cpf = input("Digite seu CPF (apenas números): ")
+    for i in usuarios:
+        if cpf in i:
+            print("Usuário já cadastrado.")
+            return
+    nome = input("Digite o nome completo:\n")
+    data_nasc = input("Digite a data denascimento no formato dd/mm/aaaa:\n")
+    endereco = input("Digite seu endereço no formato: logradouro, nmr - bairro - cidade/sigla do estado\n")
+    usuarios.append([cpf, nome, data_nasc, endereco])
 
-menu = """
-    ########## MENU ##########
-    0 - Realizar saque;
-    1 - Realizar depósito;
-    2 - Ver extrato;
-    3 - Sair.
-"""
-opcao = int(input(menu))
+def cria_conta_corr(user, usuarios, contas):
+    for i in usuarios:
+        if user in i:
+            numero_conta = len(contas) + 1
+            contas.append(["0001", numero_conta, user[0][0]])        
+            return
 
-while(True):
-    #SAQUE
-    if opcao == 0:
-        valor = float(input("Qual valor do saque?\n"))
-        retorno = saque(saldo = saldo, valor = valor, extrato = extrato, limite = limite, numero_de_saques = numero_de_saques, limite_saques = LIMITE_SAQUES)
-        saldo = retorno[0]
-        extrato = retorno[1]
-        numero_de_saques = retorno[2]
-        
-    #DEPOSITO
-    if opcao == 1:
-        valor = float(input("Qual valor do deposito?\n"))
-        retorno = deposito(saldo, valor, extrato)
-        saldo = retorno[0]
-        extrato = retorno[1]
+def main():
+    usuarios = [["123","luiz","13/03/2003", "sao joao del rei"]]
+    contas = []
 
-    #EXTRATO
-    if opcao == 2:
-        exibe_extrato(saldo, extrato = extrato)
-
-    #SAIR
-    if opcao == 3:
-        print("OBRIGADO POR USAR NOSSO SISTEMA!\n")
-        break
-    
+    menu = """
+        ########## MENU ##########
+        0 - Realizar saque;
+        1 - Realizar depósito;
+        2 - Ver extrato;
+        3 - Cadastrar Usuário;
+        4 - Criar conta corrente;
+        5 - Sair.
+    """
     opcao = int(input(menu))
 
+    while(True):
+        #SAQUE
+        if opcao == 0:
+            valor = float(input("Qual valor do saque?\n"))
+            retorno = saque(saldo = saldo, valor = valor, extrato = extrato, limite = limite, numero_de_saques = numero_de_saques, limite_saques = LIMITE_SAQUES)
+            saldo = retorno[0]
+            extrato = retorno[1]
+            numero_de_saques = retorno[2]
+            
+        #DEPOSITO
+        if opcao == 1:
+            valor = float(input("Qual valor do deposito?\n"))
+            retorno = deposito(saldo, valor, extrato)
+            saldo = retorno[0]
+            extrato = retorno[1]
 
+        #EXTRATO
+        if opcao == 2:
+            exibe_extrato(saldo, extrato = extrato)
+
+        #cadastrar usuario
+        if opcao == 3:
+            cadastra_usuario(usuarios)
+            
+
+        #criar conta
+        if opcao == 4:
+            user = input("Digite o cpf: ")
+            cria_conta_corr(user, usuarios, contas)
+            
+
+        #SAIR
+        if opcao == 5:
+            print("OBRIGADO POR USAR NOSSO SISTEMA!\n")
+            break
+        
+        opcao = int(input(menu))
+
+
+main()
